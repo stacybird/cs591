a37ad3475d4543fd0ec7fb2e53fc6cf4755c1e86
Stacy Watts <swatts@pdx.edu>
2014-10-04 13:34:14 -0700

    add file input from answers.txt, logic for file error and close before normal end

--- homework1/questioner.c
+++ homework1/questioner.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
+#include<stdlib.h>
 int main()
 {
   char name[20];
   char number[20];
   int MAGICNUMBER = 12345;
   int integer;
-
+  FILE *answers;
+  
+  answers = fopen("answers.txt", "r");
+  if (answers == NULL)
+  {
+    fprintf(stderr, "Can't open answers.txt!\n");
+    exit(1);
+  }
+  
   printf("What is your name?\n");
   scanf("%s", name);
   printf("What is the magic number, %s?\n", name);
@@ -26,6 +35,8 @@ int main()
       printf("TOO HIGH\n");
     }
   }
+
+  fclose(answers);
   
   return 0;
 }
