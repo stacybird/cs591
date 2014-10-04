b2ed07e6295d2ac15bf7a886e04941cb5593b30d
Stacy Watts <swatts@pdx.edu>
2014-10-04 14:03:43 -0700

    move open braces to same line, scan file for answers, print file output for testing

--- homework1/questioner.c
+++ homework1/questioner.c
@@ -3,35 +3,39 @@
 int main()
 {
   char name[20];
+  char username[20];
   char number[20];
-  int MAGICNUMBER = 12345;
+  int magicNumber = 12345;
   int integer;
   FILE *answers;
   
   answers = fopen("answers.txt", "r");
-  if (answers == NULL)
-  {
+  if (answers == NULL) {
     fprintf(stderr, "Can't open answers.txt!\n");
     exit(1);
   }
   
   printf("What is your name?\n");
   scanf("%s", name);
+
+  while (fscanf(answers, "%s %d", username, &magicNumber) == 2) {
+    printf("%s %d\n", username, magicNumber);
+    if (username == name) {
+      break;
+    }
+  }
+  
   printf("What is the magic number, %s?\n", name);
   scanf("%s", number);
   integer = atoi(number);
-  if (integer < MAGICNUMBER)
-  {
+  if (integer < magicNumber) {
     printf("TOO LOW\n");
   }
-  else 
-  {  
-    if (integer == MAGICNUMBER)
-      {
+  else {  
+    if (integer == magicNumber) {
         printf("CORRECT\n");
       }
-    else
-    {
+    else {
       printf("TOO HIGH\n");
     }
   }
