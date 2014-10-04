209ced98122a90b075292880c876ea277b7f79bb
Stacy Watts <swatts@pdx.edu>
2014-10-04 13:14:02 -0700

    add number input, conversion to int, and comparison logic with printf s

--- homework1/questioner.c
+++ homework1/questioner.c
@@ -2,11 +2,32 @@
 int main()
 {
   char name[20];
+  char number[20];
+  int MAGICNUMBER = 12345;
+  int integer;
+
   printf("What is your name?\n");
   //getchar();
   scanf("%s", name);
   
   printf("What is the magic number, %s?\n", name);
+  scanf("%s", number);
+  integer = atoi(number);
+  if (integer < MAGICNUMBER)
+  {
+    printf("TOO LOW\n");
+  }
+  else 
+  {  
+    if (integer == MAGICNUMBER)
+      {
+        printf("CORRECT\n");
+      }
+    else
+    {
+      printf("TOO HIGH\n");
+    }
+  }
   
   return 0;
 }
