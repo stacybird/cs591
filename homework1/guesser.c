f0d5696abd709cc775986fa5ebecc39c6ee33e17
Stacy Watts <swatts@pdx.edu>
2014-10-05 23:02:01 -0700

    first try at threaded approach with other ideas commented.  doesn't quite go yet

--- homework1/guesser.c
+++ homework1/guesser.c
@@ -1,16 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <string.h>
 int main()
 {
   char prompt[39];
   char name[7] = "Jimbob";
   int num = 12345;
+  pid_t childPid;
+  
+  childPid = fork();
+  if (childPid == -1) {
+    fprintf(stderr, "call to fork failed, exiting");
+    exit(1);
+  }
+  if (childPid == 0) { // this is the child
+    system("./a.out");
+    //execl("/bin/sh", "-c", "~/a.out", NULL);
+  }
+  else {
+    sleep(1);
+    printf("%s\n", name);
+    sleep(1);
+    //fprintf(stderr,"%s\n", name);
+    printf("%d\n", num);
+    fprintf(stderr,"%d\n", num);
+    //system("strace -p %d", childPid);
+    //printf("%d", childPid);
+    //execl("./", "a.out", NULL);
 
-  printf("%s\n", name);
-  fprintf(stderr,"%s\n", name);
-  printf("%d\n", num);
-  fprintf(stderr,"%d\n", num);
+  }
+
+// strace  -p $child 2>&1| sed -re 's%^write\(1,[[:blank:]](.*),[[:blank:]]*[0-9]+\)[[:blank:]]*=[[:blank:]]*[0-9]+%\1%g'
+
 
   return 0;
 }
