7466115cf46dd7616af4447e5b276226bf209288
Stacy Watts <swatts@pdx.edu>
2014-10-06 22:35:04 -0700

    add simple starting fgets without checks

--- homework1/guesser.c
+++ homework1/guesser.c
@@ -3,11 +3,17 @@
 #include <string.h>
 int main()
 {
-  char prompt[39];
+  char EXPECT1[39] = "What is your name?";
+  char EXPECT2[39] = "What is the magic number, Jimbob?";
+  char prompt1[39];
+  char prompt2[39];
+  int MAX = 39;
   char name[7] = "Jimbob";
   int num = 12345;
-  
+ 
+  fgets (prompt1, MAX, stdin);
   printf("%s\n", name);
+  fgets (prompt2, MAX, stdin);
   printf("%d\n", num);
 
 
